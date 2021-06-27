ede7724b2e852fef450af90c19e19cacc9b87f2e
eswhat93 <adleeeunsol@gmail.com>
2021-06-27 12:40:35 +0900

    add lineChart

---
+++ src/chart/chart.js
@@ -0,0 +1,15 @@
+// npm install chart.js@2.9.4 => 상위버전 에러로 다운그레이드해서 다운
+
+import { Line, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
+
+export default {
+  extends: Line,
+  mixins: [reactiveProp],
+  props: ['options'],
+  mounted () {
+    // this.chartData is created in the mixin.
+    // If you want to pass options please create a local options object
+    this.renderChart(this.chartData, this.options)
+  }
+}
