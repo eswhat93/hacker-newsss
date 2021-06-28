fe051abe55f0c53b3c0428a6ede2c0e503d13ea1
eswhat93 <adleeeunsol@gmail.com>
2021-06-28 10:29:42 +0900

    meaningless

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
     <ToolBar></ToolBar>
-    <LineChart></LineChart>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 import ToolBar from './components/ToolBar.vue'
-import LineChart from './components/LineChart.vue'
 export default {
   components:{
     ToolBar,
-    LineChart
   },
 }
 </script>
