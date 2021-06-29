773566bf162727e1127647c3ad672e021a1a5c0f
eswhat93 <adleeeunsol@gmail.com>
2021-06-29 17:41:46 +0900

    유저 페이지 추가

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
-      user
+    <div :if="this.$store.state.user">
+      <p>{{userInfo.id}}</p>
+      <p>{{userInfo.karma}}</p>
+      <p>{{userInfo.created}}</p>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
 
+export default {
+  computed:{
+    userInfo(){
+      return this.$store.state.user
+    }
+  },
+  created(){
+    const userName = this.$route.params.id; 
+    this.$store.dispatch('FETCH_USER', userName)
+  }
 }
 </script>
 
