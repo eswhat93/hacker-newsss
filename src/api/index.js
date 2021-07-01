f742f77744299e94c25ff7e7286dbf7925d95dde
eswhat93 <adleeeunsol@gmail.com>
2021-07-01 17:56:47 +0900

    HOC에 따른 코드정리

--- src/api/index.js
+++ src/api/index.js
@@ -8,18 +8,6 @@ const config={
 }
 
 //2. API 함수들 정리
-function fetchNewsList(){
-    return axios.get(`${config.baseUrl}news/1.json`)
-}
-
-function fetchJobsList(){
-    return axios.get(`${config.baseUrl}jobs/1.json`)
-}
-
-function fetchAskList(){
-    return axios.get(`${config.baseUrl}ask/1.json`)
-}
-
 function fetchUserInfo(userName){
     return axios.get(`${config.baseUrl}user/${userName}.json`)
 }
@@ -32,9 +20,6 @@ function fetchList(pageName){
 }
 
 export{
-    fetchNewsList,
-    fetchJobsList,
-    fetchAskList,
     fetchUserInfo,
     fetchItem,
     fetchList
