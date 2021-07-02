83fee2185d2cbe70e187f60095bfcb56c626f1f9
eswhat93 <adleeeunsol@gmail.com>
2021-07-02 17:58:51 +0900

    믹스인 추가, 스피너 엔드 호출시기 변경

--- src/mixins/ListMixin.js
+++ src/mixins/ListMixin.js
@@ -1,9 +1,26 @@
+import bus from '../utils/bus.js'
+
 export default{
-    //재사용할 컴포넌트 옵션
-    created(){
-        
+    mounted(){
+        bus.$emit('end:spinner');
     }
+    //재사용할 컴포넌트 옵션
+    // created(){
+    //     bus.$emit('start:spinner');
+    //     //  setTimeout(()=>{
+    //         //1번째
+    //         console.log(1);
+    //         this.$store.dispatch('FETCH_LIST', this.$route.name)
+    //         .then(()=>{
+    //             //5번째
+    //             console.log(5);
+    //             bus.$emit('end:spinner')
+    //         })
+    //         .catch(error => {
+    //             console.log(error);
+    //         })
+    //     //  },3000)
+        
+    // }
 }
-
-
 
