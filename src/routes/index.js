7de438b9ee05a1c07a6e24fadf3e2215b96fd4e5
eswhat93 <adleeeunsol@gmail.com>
2021-07-02 17:59:51 +0900

    mixin => 라우터 네비게이션 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import UserView from '../views/UserView.vue'
 import ItemView from '../views/ItemView.vue'
+import NewsView from '../views/NewsView.vue'
+import JobsView from '../views/JobsView.vue'
+import AskView from '../views/AskView.vue'
 
-import createListView from '../views/CreateListView.js'
+// import createListView from '../views/CreateListView.js'
+import bus from '../utils/bus'
+import { store } from '../store'
 
 //npm i vue-router --save 
 Vue.use(VueRouter);
@@ -22,20 +27,47 @@ export const router = new VueRouter({
             path:'/news',
             //url에 해당하는 컴포넌트
             name:'news',
-            //component:NewsView,
-            component:createListView('NewsView')
+            component:NewsView,
+            //component:createListView('NewsView'),
+            beforeEnter:(to, from, next)=>{
+                //to, from , next
+                bus.$emit('start:spinner');
+                store.dispatch('FETCH_LIST', to.name)
+                .then(()=>{
+                    // bus.$emit('end:spinner');
+                    next();
+                })
+                .catch(error =>{
+                    console.log(error);
+                })
+            }
         },
         {
             path:'/ask',
             name:'ask',
-            //component:AskView,
-            component:createListView('AskView')
+            component:AskView,
+            //component:createListView('AskView'),
+            beforeEnter:(to, from, next)=>{
+                //to, from , next
+                bus.$emit('start:spinner');
+                store.dispatch('FETCH_LIST', to.name)
+                .then(()=>{
+                    // bus.$emit('end:spinner');
+                    next();
+                })
+                .catch(error =>{
+                    console.log(error);
+                })
+            }
+            
+            
         },
         {
             path:'/jobs',
             name:'jobs',
-            //component:JobsView,
-            component:createListView('JobsView')
+            component:JobsView,
+            //component:createListView('JobsView')
+            
         },
         {
             path:'/user/:id',
