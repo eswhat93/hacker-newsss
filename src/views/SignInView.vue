e54eff24a7278fe561e737b76a19432ac2f21213
eswhat93 <adleeeunsol@gmail.com>
2021-07-05 18:11:02 +0900

    try catch 추가

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -10,6 +10,7 @@
 
 <script>
 import axios from 'axios'
+import {handleException} from '../utils/handler.js'
 export default {
     data(){
         return {
@@ -18,11 +19,18 @@ export default {
     },
     methods:{
         async loginUser1(){
-            var response = await axios.get('https://jsonplaceholder.typicode.com/users/1');
-            if(response.data.id === 1){
-                var list = await axios.get('https://jsonplaceholder.typicode.com/todos');
-                this.items = list.data;
+            //try catch는 자바스크립트 코드 에러 처리까지 폭넓게 보여줘서 더 포괄적으로 예외처리를 할 수 있음
+            try {
+                var response = await axios.get('https://jsonplaceholder.typicode.com/users/1');
+                if(response.data.id === 1){
+                    var list = await axios.get('https://jsonplaceholder.typicode.com/todos');
+                    this.items = list.data;
+                }
+            } catch (error) {
+                handleException(error);
+                console.log(error);
             }
+            
         }
     }
 }
