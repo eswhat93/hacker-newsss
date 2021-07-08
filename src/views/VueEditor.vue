79acfc627ff402bd58f0efae1098b8799d074830
eswhat93 <adleeeunsol@gmail.com>
2021-07-08 17:49:41 +0900

    vue-editor 구현

---
+++ src/views/VueEditor.vue
@@ -0,0 +1,18 @@
+<template>
+    <vue-editor></vue-editor>
+</template>
+
+<script>
+import { VueEditor } from "vue2-editor";
+
+export default {
+    components:{
+        VueEditor,
+        // Quill
+    }
+}
+</script>
+
+<style>
+
+</style>
